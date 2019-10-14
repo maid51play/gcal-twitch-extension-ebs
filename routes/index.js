a73c5e08c294139fcdedae3f815356955c0a5d5d
MeganeMidori <MeganeMidori@users.noreply.github.com>
2019-10-14 14:25:55 -0700

    return schedule from env vars

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
 var express = require('express');
 var router = express.Router();
 
@@ -7,7 +9,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/schedule', function(req, res, next) {
-  res.status(200).send("schedule goes here")
+  const calendarId = process.env.CALENDAR_ID
+  const apiKey = process.env.API_KEY
+  const maxResults = 7
+  const timeMin = `${new Date().toISOString()}`;
+
+  fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&maxResults=${maxResults}&timeMin=${timeMin}&orderBy=startTime&singleEvents=true`)
+  .then(res => {
+    res.status(200).send("schedule goes here")
+  })
+  .catch(err => {
+    res.status(500).send("something went wrong")
+  })
 })
 
 module.exports = router;
