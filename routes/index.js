5c645d8db63c97697b55dd7c19a1e6ca8050971f
MeganeMidori <MeganeMidori@users.noreply.github.com>
2019-10-14 14:54:25 -0700

    return event info

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/schedule', function(req, res, next) {
+router.get('/schedule', async function(req, res, next) {
   const calendarId = process.env.CALENDAR_ID
   const apiKey = process.env.API_KEY
   const maxResults = 7
   const timeMin = `${new Date().toISOString()}`;
 
-  fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&maxResults=${maxResults}&timeMin=${timeMin}&orderBy=startTime&singleEvents=true`)
-  .then(res => {
-    res.status(200).send("schedule goes here")
-  })
-  .catch(err => {
+  console.log(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&maxResults=${maxResults}&timeMin=${timeMin}&orderBy=startTime&singleEvents=true`)
+
+
+  try {
+    const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&maxResults=${maxResults}&timeMin=${timeMin}&orderBy=startTime&singleEvents=true`)
+    const json = await response.json()
+    res.status(200).send(json.items.map(event => ({title: event.summary, startDateTime: event.start.dateTime})))
+  } catch(err) {
+    console.log(err)
     res.status(500).send("something went wrong")
-  })
+  }
 })
 
 module.exports = router;
