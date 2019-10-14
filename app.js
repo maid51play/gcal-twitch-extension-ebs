5b90374f26b40e5bab8403dcc4690bc483c5bd8c
MeganeMidori <MeganeMidori@users.noreply.github.com>
2019-10-14 13:01:32 -0700

    require cors

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
